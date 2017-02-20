df05b923166a853bc5488d77b8b0f628753b081a
Gerald Albion <gerald.albion@royalroads.ca>
2017-02-20 09:02:27 -0800

    [MOOCR-3557] Group resolution

--- sources/instructors_mm_enrol_rru_source.php
+++ sources/instructors_mm_enrol_rru_source.php
@@ -18,10 +18,11 @@
 /**
  * RRU enrolment , enrol instructors as students in Mastering Moodle Course
  *
- * 2012-09-12
+ * 2017-02-14
  * @package    enrol
  * @subpackage rru
- * @copyright  2012 Andrew Zoltay, Royal Roads University
+ * @author     Gerald Albion, Andy Zoltay
+ * @copyright  © 2017 Royal Roads University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -44,6 +45,7 @@ class instructors_mm_enrol_rru_source extends enrol_rru_source {
     public function get_enrolment_settings(){
         return(array());
     }
+    
     /**
      * Get the current enrolments and course identifiers from the SIS
      *
@@ -59,12 +61,200 @@ class instructors_mm_enrol_rru_source extends enrol_rru_source {
         return $enrolments;
     }
 
+    /**
+     * Fetches all roles the user has in all contexts
+     * 
+     * @param int $userid
+     * @return mixed array The roleids or bool false if error
+     * global $DB the Moodle DML API object
+     */
+    private function getuserroles($userid) {
+    	// We need a distinct list of roles held by the user in all contexts.
+    	$result = $DB->get_records_sql('SELECT DISTINCT roleid FROM {role_assignments} WHERE userid = ?', array($userid));
+    	
+    	// If something went wrong that was not an exception, return false.
+    	if (!$result) {
+    		return false;
+    	}
+    	
+    	// We want to return a simple array of integers, not the array of objects the query returned.
+    	$return = array();
+    	foreach ($result as $id->data) {
+    		$return[] = $id;
+    	}
+    	return $return;
+    }
 
+    /**
+     * Resolves a category name into a short school code used in the group's idnumber field.
+     * @param str $catname the category name
+     * @return str the school code or bool false if not found;
+     */
+    private function getschoolcode($catname) {
+    	switch ($cat) {
+    		case 'CCWI'         : return 'CCWI'; 
+    		case 'INTRDISCPL'   : return 'CIS'; 
+    		case 'ISC'          : return 'ISC'; 
+    		case 'COMM-CULTURE' : return 'SCC'; 
+    		case 'ENVIRONMENT'  : return 'SES'; 
+    		case 'EDUTECH'      : return 'SET'; 
+    		case 'CONFLICT'     : return 'SHS'; 
+    		case 'MGMT-GRAD'    :
+    		case 'MGMT-UGRAD'   : return 'SOB'; 
+    		case 'LEADERSHIP'   : return 'SOLS';
+    		case 'TOURHOSPMGMT' : return 'STHM';
+    		default             : return false;
+    	}
+    }
+    
+    /**
+     * Fetches all the schools in which the user has some kind of instructor role
+     * 
+     * @param int $userid ID of the user in question
+     * @return mixed array the schools (array of strings) or bool false if error
+     * @global $DB the Moodle DML API object
+     */
+    private function getuserschools($userid) {
+    	// Get all of the course categories and parent categories in which the user has a non-student role.
+    	$sql = "
+SELECT DISTINCT cc.name AS 'category', cp.name AS 'parentcat'
+     FROM mdl_user u 
+LEFT JOIN mdl_role_assignments ra ON u.id = ra.userid        
+LEFT JOIN mdl_role r ON r.id = ra.roleid
+LEFT JOIN mdl_context cx ON cx.id = ra.contextid
+LEFT JOIN mdl_course co ON co.id = cx.instanceid
+LEFT JOIN mdl_course_categories cc ON cc.id = co.category
+LEFT JOIN mdl_course_categories cp ON cp.id = cc.parent
+    WHERE u.idnumber = ? 
+      AND cx.contextlevel = 50 
+      AND ra.roleid <> 5";    	
+    	$resultset = $DB->get_records_sql($sql, array($userid));
+    	
+		$result = array();
+		
+    	// Iterate the found categories for this user.
+    	foreach($resultset as $row) {
+    		
+    		// Try to resolve the school from the course category.
+    		$schoolcode = getschoolcode($row->category);
+    		
+    		// Did this fail?  Try again with the course's parent category.
+    		if (!$schoolcode) {
+    			$schoolcode = getschoolcode($row-parentcat);
+    		}
+    		
+    		// Still no luck?  We don't recognize this category.  It might be a test category or 
+    		// something else not associated with a school.  Skip it.
+    		if (!$schoolcode) { // No recognized categories or parent categories.
+    			continue;
+    		}
+    		
+    		// We have a school code (as used in the course groups' idnumber column).  Add it to the list.
+    		$result[]=$schoolcode;    		
+    	}
+    	
+    	// Return false if there are no schools for this user.
+    	if (empty($result)) {
+    		return false;
+    	} else {
+    		return $result;
+    	}
+    }
+    
+    /**
+     * Assign users to groups within Orientation to Teaching & Design
+     * Based on the user's school (school of the course they teach) or role in the university.
+     * @author Gerald Albion
+     * date	   2017-02-17
+     * 
+     * @param  array $users 
+     * @return boolean false on failure
+     * @global object $DB The Moodle DML API object
+     */
+    private function assigngroups($users) {
+    	// Init group memberships.
+    	$group                    = array();
+    	$group['DEANORDESIGNATE'] = array(); // Deans - not currently used here.  Populate this group manually.
+    	$group['PROGRAMHEADS']    = array(); // Program heads (Program leads)
+    	$group['PROGRAMSTAFF']    = array(); // Program staff (PAs)
+    	$group['SCHOOLDIRECTORS'] = array(); // Directors - not currently used here.  Populate this group manually.
+    	$group['OTHERRRUSTAFF']   = array(); // Other RRU staff.  Library, Help Desk, CTET Admin and anyone without any other role or school.
+    	$group['CCWI']            = array(); // CCWI
+    	$group['CIS']             = array(); // Interdiciplinary Studies
+    	$group['ISC']             = array(); // ISC
+    	$group['SCC']             = array(); // Comm & Culture
+    	$group['SES']             = array(); // Environment
+    	$group['SET']             = array(); // EduTech
+    	$group['SHS']             = array(); // Humanitarian studies / conflict
+    	$group['SOB']             = array(); // School of Business
+    	$group['SOLS']            = array(); // Leadership
+    	$group['STHM']            = array(); // Tourism Hospitality Management
+    	
+    	// Build a list of users for each group.
+    	foreach($users as $id => $user) { // id is user id, always same as $user->teacher
+
+			// Init flag.
+    		$foundgroup=false;
+			
+    		// Role groups
+    		$roles = $this->getuserroles($id);
+    		foreach ($roles as $roleid) {
+    			switch ($roleid) {
+    				case 12 : // Program Associate = "Program Staff" group
+    					$group['PROGRAMSTAFF'][]=$id;
+    					$foundgroup = true;
+    					break;
+    				case 15:
+    				case 17: // Programlead-director or programlead_directorisc = "Program Heads" group
+    					$group['PROGRAMHEADS'][]=$id;
+    					$foundgroup = true;
+    					break;
+    				case 9:
+    				case 11:
+    				case 13: // CTETAdmin, Library, Help Desk = "Other RRU Staff" group
+    					$group['OTHERRRUSTAFF'][]=$id;
+    					$foundgroup = true;
+    					break;
+    				default: // No default here.
+    			}
+    		}
+    		
+    		// School groups
+    		$schools = $this->getuserschools($id);
+    		foreach($schools as $school) {
+    			switch ($school) {
+    				case 'CCWI':
+   					case 'CIS':
+   					case 'ISC':
+   					case 'SCC':
+   					case 'SES':
+   					case 'SET':
+   					case 'SHS':
+   					case 'SOB':
+   					case 'SOLS':
+   					case 'STHM':
+   						$group[$school][]=$id;
+   						$foundgroup = true;
+   						break;
+    			}
+    		}    		
+    		 
+    		// If no roles or schools, add "Other RRU Staff"
+    		if (!$foundgroup) {
+    			$group_other[]=$id;
+    		}
+    		
+    	}
+    	// Get group ids for the course.
+    	
+    	// Assign groups.
+    }
+    
     /**
      * Get the current enrolments and course identifiers from the SIS
      *
-     * @author Andrew Zoltay
-     * date    2012-09-12
+     * @author Gerald Albion, Andrew Zoltay
+     * updated 2017-02-14
      * @global object $DB - Moodle database object
      * @return array of db records or false if orror occurs
     */
@@ -72,38 +262,57 @@ class instructors_mm_enrol_rru_source extends enrol_rru_source {
         global $DB;
 
         try {
-            //Get a list of all instructors.
+            //Get a list of all instructors (of all kinds), program staff, approvers, and other RRU staff who should be enrolled.
             $sql = "
-              SELECT
-              DISTINCT usr.idnumber AS teacherid
-              FROM mdl_course c
-              INNER JOIN mdl_context cx
-              ON c.id = cx.instanceid
-              AND cx.contextlevel = '50'
-              INNER JOIN mdl_role_assignments ra
-              ON cx.id = ra.contextid
-              INNER JOIN mdl_role r ON ra.roleid = r.id
-              INNER JOIN mdl_user usr ON ra.userid = usr.id
-              WHERE r.archetype = 'editingteacher'
-              AND c.idnumber  !=''
-              AND usr.idnumber !=''
-              ORDER BY c.fullname, usr.lastname;
+SELECT DISTINCT ra.userid AS teacherid
+           FROM mdl_role_assignments ra
+     INNER JOIN mdl_context cx ON cx.id = ra.contextid
+     INNER JOIN mdl_course co ON co.id = cx.instanceid
+     INNER JOIN mdl_user u ON u.id = ra.userid
+          WHERE roleid IN (3, 4, 9, 10, 12, 13, 15, 17, 18, 20) -- editing, non-editing, live course instructors; CTET Admins; Approvers; Help Desk; Program Directors; Program Leads; PAs
+            AND cx.contextlevel in (40, 50) -- course or category
+            AND co.idnumber <> '' -- course must have an idnumber value
+            AND u.idnumber <> '' -- user idnumber must not be blank
+       ORDER BY co.fullname, u.lastname; 
              ";
 
             $result = $DB->get_records_sql($sql);
         }
         catch(dml_exception $e) {
-            $this->write_log("Failed to get approvers from database - DB error: [$e->debuginfo]", true);
+            $this->write_log("Failed to get course participants from database - DB error: [$e->debuginfo]", true);
             return false;
         }
-        $masteringmoodleid = $DB->get_field('course', 'idnumber', array('fullname'=>'Mastering Moodle'), MUST_EXIST);
+        
+        /*
+         * It may seem a bit like putting the cart before the horse to assign users to groups in the course now, before the users are 
+         * actually enrolled, but this is how we can keep this special case for this enrolment source in the instructors_mm submodule.  
+         */
+        $this->assigngroups($result);
+        
+        /*
+         * Prepare the enrolments output.
+         */ 
+        $coursename = 'Orientation to Teaching & Course Design';
+        try { 
+        	$courseid = $DB->get_field('course', 'idnumber', array('fullname'=>$coursename), MUST_EXIST);
+        } catch (Exception $e) {
+        	$this->write_log("Unable to get idnumber for '{$coursename}'.\nException message: " . $e->getMessage() . "\n");
+        	$this->write_log("Instructor and staff enrolments in {$coursename} will NOT be updated.\n");
+        }
+        
+        if (!$courseid) {
+        	$this->write_log("'{$coursename}' exists but unable to get idnumber.  Make sure the course has a non-blank idnumber.\n");
+        	$this->write_log("Instructor and staff enrolments in {$coursename} will NOT be updated.\n");
+        }
+               
         $studentroleid = $DB->get_field('role', 'id', array('archetype'=>'student'), MUST_EXIST);
         // Get the data in the correct format for enrol_rru plugin to deal with it.
         $enrolments = array();
+                
         foreach($result as $id => $record) {
             // Format enrolments.
             $enrolment = array();
-            $enrolment['chrCourseCode'] = $masteringmoodleid;
+            $enrolment['chrCourseCode'] = $courseid;
             $enrolment['intUserCode'] = $record->teacherid;
             $enrolment['intRoleID'] = $studentroleid;
 
